b05c234c6ccadbd5289db89c915b3d422aa64eb8
AndekeX <35708345+AndekeX@users.noreply.github.com>
2020-03-22 00:30:13 +0100

    Yup

---
+++ UniEconomy/UniEconomyAPIRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniEconomyAPI
+{
+    public class UniEconomyAPIRequest
+    {
+        public Login.UniEconomy_Login Login;
+        public Modules.Modules Modules;
+
+        public UniEconomyAPIRequest(Dictionary<string, string> Arg_AuthData)
+        {
+            this.Modules = new Modules.Modules(Arg_AuthData);
+        }
+
+        public UniEconomyAPIRequest()
+        {
+            Login = new Login.UniEconomy_Login();
+        }
+    }
+}
